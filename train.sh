38204c0e17b84b0954cec4e3cde0c1bf393951f8
gabormarko <gabor.marko@tum.de>
2025-05-28 20:01:44 +0200

    Test

--- train.sh
+++
@@ -1,8 +0,0 @@
-mkdir output
-output_dir="./output/unifed_lift/"
-mkdir ${output_dir}
-
-scenes=("figurines" "ramen" "teatime")
-for index in "${!scenes[@]}"; do
-    python train_unified_lift.py -s ./data/lerf/${scenes[$index]} -m ${output_dir}/${scenes[$index]}   --config_file config/gaussian_dataset/train.json --weight_loss 1e-0 --train_split --use_wandb
-done
