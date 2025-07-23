5847829511b32a43647feb47980c87754a9488b9
gabormarko <gabor.marko@tum.de>
2025-07-24 01:23:48 +0200

    final version, correct pinhole camera model used for 3DGS and projection:)

--- voxel_to_gaussian/render_semantics_logits.sh
+++ voxel_to_gaussian/render_semantics_logits.sh
@@ -3,12 +3,14 @@
 # Usage: bash render_semantics.sh
 
 # --------- User Parameters ---------
-MODEL_PATH="/home/neural_fields/gaussian-splatting/output/cc946f8b-f"   # Path to model directory
+#MODEL_PATH="/home/neural_fields/gaussian-splatting/output/cc946f8b-f"   # Path to model directory
+MODEL_PATH="/home/neural_fields/gaussian-splatting/output/39ac5c9a-1"   # Path to model directory
+#MODEL_PATH="/home/neural_fields/gaussian-splatting/output/f79056cd-8"
 #MODEL_PATH="/home/neural_fields/gaussian-splatting/output/237ab5b3-5"
 #MODEL_PATH="/home/neural_fields/Unified-Lift-Gabor/betterGaussians/filtered_gaussians.ply"
 
 ITERATION=-1                           # Checkpoint iteration (-1 for latest)
-GAUSSIAN_LABELS="/home/neural_fields/Unified-Lift-Gabor/voxel_to_gaussian/gaussian_semantics_filt_gauss_45000.npz"  # Path to semantic logits npz
+GAUSSIAN_LABELS="/home/neural_fields/Unified-Lift-Gabor/voxel_to_gaussian/gaussian_semantics_filt_gauss_195120_30000_NEW.npz"  # Path to semantic logits npz
 SKIP_TRAIN=false                       # Set to true to skip train rendering
 SKIP_TEST=false                        # Set to true to skip test rendering
 QUIET=false                            # Set to true for quiet mode
